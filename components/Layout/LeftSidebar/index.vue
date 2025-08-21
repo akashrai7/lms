c7d5acf75528aa7fe97e07d142275061e24654b7
Akash Rai <90545283+akashrai7@users.noreply.github.com>
2025-08-21 14:51:23 +0530

    Update index.vue

--- components/Layout/LeftSidebar/index.vue
+++ components/Layout/LeftSidebar/index.vue
@@ -5,7 +5,7 @@
         to="/dashboard"
         class="d-block text-decoration-none position-relative"
       >
-        <img src="~/assets/images/mono.png" alt="logo-icon" width="30%" />
+        <img src="~/assets/images/mono.png" alt="logo-icon" width="10%" />
         <span class="logo-text fw-bold text-dark">AI GURUKL</span>
       </NuxtLink>
       <button
