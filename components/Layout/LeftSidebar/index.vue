accf575aa98256d7f4e0a1b2124b310209047045
Akash Rai <90545283+akashrai7@users.noreply.github.com>
2025-08-21 13:26:56 +0530

    Update header gurukl name

--- components/Layout/LeftSidebar/index.vue
+++ components/Layout/LeftSidebar/index.vue
@@ -6,7 +6,7 @@
         class="d-block text-decoration-none position-relative"
       >
         <img src="~/assets/images/logo-icon.png" alt="logo-icon" />
-        <span class="logo-text fw-bold text-dark">Gurukul</span>
+        <span class="logo-text fw-bold text-dark">AI GURUKL</span>
       </NuxtLink>
       <button
         class="sidebar-burger-menu bg-transparent p-0 border-0 opacity-0 z-n1 position-absolute top-50 end-0 translate-middle-y"
